9534722e242c9bfb04ffab4d579056f47e174ea1
Matt Emson <memson@gmail.com>
2017-09-05 12:51:13 +0100

    Initial checkin
    This includes all the changes from Blazor that allow Core apps to be run via DNA.

---
+++ testapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
