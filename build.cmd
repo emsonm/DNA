afb24d18b9dd11128057b4169bc6270675d54845
Matt Emson <memson@gmail.com>
2017-09-11 13:47:46 +0100

    vscode
    made vscode more or less work with the source. MSBuild is horrible though.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" 
-msbuild.exe  
+msbuild.exe ".\dna.sln" /t:rebuild /property:Configuration=Release /property:Platform=x86
 
--- clean.cmd
+++ clean.cmd
@@ -1,4 +1,5 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" 
-msbuild.exe  
+msbuild.exe ".\corelib\corelib.csproj" /t:clean
+msbuild.exe ".\native\dna.vcxproj" /t:clean
 
